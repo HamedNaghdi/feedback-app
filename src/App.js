5df3d34b54e6cf02c7cc0cffc58ada9a2395d219
Hamed Naghdi <hamed.naghdi@hotmail.com>
2022-07-06 22:21:09 +0430

    026-Creating Links v5 and v6

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
+import AboutIconLink from "./components/AboutIconLink";
 import AboutPage from "./pages/AboutPage";
 
 function App() {
@@ -43,6 +44,7 @@ function App() {
                     } />
                     <Route path="/about" element={<AboutPage />} />
                 </Routes>
+                <AboutIconLink />
             </div>
         </Router>
     );
