dbcc4c92d8c4b3a344110eb6e5e0f38f820947b8
Hamed Naghdi <hamed.naghdi@hotmail.com>
2022-07-06 21:17:42 +0430

    025-Upgrading to React Router 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from "react";
 import { v4 as uuidv4} from "uuid"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Routes,
+    Link
+  } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,15 +30,21 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header/>
             <div className="container">
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                <Routes>
+                    <Route exact path="/" element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback}/>
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                        </>
+                    } />
+                    <Route path="/about" element={<AboutPage />} />
+                </Routes>
             </div>
-
-        </>
+        </Router>
     );
 }
 
