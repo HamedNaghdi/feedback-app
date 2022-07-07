a642528ace9e66f603e7d4e617818e67a6898edc
Hamed Naghdi <hamed.naghdi@hotmail.com>
2022-07-07 11:37:25 +0430

    029-Create a Context and Provider

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import {
 import Card from "./components/shared/Card";
 import { useState } from "react";
 import Header from "./components/Header"
+
+import { FeedbackProvider } from "./context/FeedbackContext";
+
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
@@ -35,34 +38,36 @@ function App() {
     }
 
     return (
-        <Router>
-            <Header/>
-            <div className="container">
-                <Routes>
-                    <Route exact path="/" element={
-                        <>
-                            <FeedbackForm handleAdd={addFeedback}/>
-                            <FeedbackStats feedback={feedback} />
-                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-                        </>
-                    } />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/post/:slug/:name" element={<Post />} />
-                    <Route path="/blog/*" element={<Blog />} />
-                </Routes>
+        <FeedbackProvider>
+            <Router>
+                <Header/>
+                <div className="container">
+                    <Routes>
+                        <Route exact path="/" element={
+                            <>
+                                <FeedbackForm handleAdd={addFeedback}/>
+                                <FeedbackStats feedback={feedback} />
+                                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                            </>
+                        } />
+                        <Route path="/about" element={<AboutPage />} />
+                        <Route path="/post/:slug/:name" element={<Post />} />
+                        <Route path="/blog/*" element={<Blog />} />
+                    </Routes>
 
-                <Card>
-                    <NavLink to="/" activeClassName="active">
-                        Home
-                    </NavLink>
-                    <NavLink to="/about" activeClassName="active">
-                        About
-                    </NavLink>
-                </Card>
+                    <Card>
+                        <NavLink to="/" activeClassName="active">
+                            Home
+                        </NavLink>
+                        <NavLink to="/about" activeClassName="active">
+                            About
+                        </NavLink>
+                    </Card>
 
-                <AboutIconLink />
-            </div>
-        </Router>
+                    <AboutIconLink />
+                </div>
+            </Router>
+        </FeedbackProvider>
     );
 }
 
